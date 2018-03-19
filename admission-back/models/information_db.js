d8bce5aa80af7f431d4ac25c96669f1fa4aa63bc
TimDeng <milkybird98@outlook.com>
2018-03-19 23:22:04 +0800

    check and update

--- admission-back/models/information_db.js
+++ admission-back/models/information_db.js
@@ -14,23 +14,6 @@ MongoClient.connect(ConfigSet.DATABASE_URL, (err, client) => {
     }
 });
 
-exports.addInformation = async function(StudentData,res) {
-    let collection = db.collection(ConfigSet.COLLECTION_NAME);
-    let result =await collection.insertOne({
-        name: StudentData.name,
-        sex: StudentData.sex,
-        class: StudentData.class,
-        phone: StudentData.phone,
-        FirstExcept: StudentData.FirstExcept,
-        SecondExcept: StudentData.SecondExcept,
-        AdjustedOrNot: StudentData.AdjustedOrNot,
-        SelfIntroduction: StudentData.SelfIntroduction,
-    });
-    // let result = collection.find({name: StudentData.name});
-    // console.log(result.ops[0]);
-    return result.ops[0]
-};
-
 exports.getCollection = function(){
     return db.collection(ConfigSet.COLLECTION_NAME);
 };
--- admission-back/routes/information.js
+++ admission-back/routes/information.js
@@ -9,6 +9,7 @@ router.post('/', urlencodedParser, async function (req, res, next) {
     // 获取req.body传来的信息，暂存在StudentData中
     let StudentData = {
         name: req.body.name,
+        uid: req.body.uid,
         sex: req.body.sex,
         class: req.body.class,
         phone: req.body.phone,
@@ -22,7 +23,6 @@ router.post('/', urlencodedParser, async function (req, res, next) {
     // console.log(_validateStudentData(StudentData));
     // 函数验证StudentData是否符合规范，不符合则返回400（请求错误） 
     // 同是验证是否存在重复录入的情况
-
     if (!_validateStudentData(StudentData)) {
         res.status(400).send("Empty");
     } else {
@@ -32,6 +32,7 @@ router.post('/', urlencodedParser, async function (req, res, next) {
             if (!data) {
                 collection.insertOne({
                     name: StudentData.name,
+                    uid: StudentData.uid,
                     sex: StudentData.sex,
                     class: StudentData.class,
                     phone: StudentData.phone,
@@ -39,12 +40,30 @@ router.post('/', urlencodedParser, async function (req, res, next) {
                     SecondExcept: StudentData.SecondExcept,
                     AdjustedOrNot: StudentData.AdjustedOrNot,
                     SelfIntroduction: StudentData.SelfIntroduction,
-                },function(){
+                }, function () {
                     res.status(200).send("Success");
                 });
             }
             else {
-                res.status(400).send("Dump");
+                console.log(data);
+                if (req.body.uid == data.uid) {
+                    collection.save({
+                        "_id": data._id,
+                        "name": StudentData.name,
+                        "uid": StudentData.uid,
+                        "sex": StudentData.sex,
+                        "class": StudentData.class,
+                        "phone": StudentData.phone,
+                        "FirstExcept": StudentData.FirstExcept,
+                        "SecondExcept": StudentData.SecondExcept,
+                        "AdjustedOrNot": StudentData.AdjustedOrNot,
+                        "SelfIntroduction": StudentData.SelfIntroduction,
+                    }, function () {
+                        res.status(200).send("Change Successfully");
+                    });
+                } else {
+                    res.status(400).send("Dump");
+                }
             }
         });
     };
@@ -52,6 +71,8 @@ router.post('/', urlencodedParser, async function (req, res, next) {
 // 验证StudentData是否格式合法，具有必须字段
 function _validateStudentData(StudentData) {
     if (StudentData.name === undefined)
+        return false;
+    if (StudentData.uid === undefined)
         return false;
     if (StudentData.sex === undefined)
         return false;
