ef36b0d6bbd5273e2f0d69992af447f223beb72d
genius-rabbit <U201613566@hust.edu.com>
2018-03-20 21:34:31 +0800

    route

--- admission-front/src/router/index.js
+++ admission-front/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Front from '@/components/Front'
+Vue.use(Router);
 
-Vue.use(Router)
+import Home from '../components/Home'
+import Form from '../components/Form'
+import AllDepIntro from '../components/AllDepIntro'
+import Display from '../components/Display'
 
-export default new Router({
-  routes: [
+export default [
     {
-      path: '/',
-      name: 'Front',
-      component: Front
+      path: '/Home',component:Home
+    },
+    {
+      path: '/AllDepintro',component:AllDepIntro
+
+    },
+    {
+      path: '/Display',component:Display
+    },
+    {
+      path: '/Form',component:Form
     }
-  ]
-})
+    ]
