b921f85b5c9ee371e9c871937e0c775154607351
genius-rabbit <U201613566@hust.edu.com>
2018-03-20 14:45:27 +0800

    fix card

---
+++ admission-front/src/components/AllDepIntro.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-row class="row">
+
+      <el-card :body-style="{ padding: '50px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 14px;">
+          <span>部门1</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '50px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 14px;">
+          <span>部门2</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '50px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 14px;">
+          <span>部门3</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '50px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 14px;">
+          <span>部门4</span>
+
+        </div>
+      </el-card>
+  </el-row>
+
+</template>
+
+<script>
+  import ElRow from "element-ui/packages/row/src/row";
+
+  export default {
+    components: {ElRow},
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style scoped>
+  .card{
+    border-radius: 20px;
+    margin: 60px;
+    width:300px;
+    height: 300px;
+    float: left;
+  }
+</style>
--- admission-front/src/components/Display.vue
+++ admission-front/src/components/Display.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-carousel :interval="4000" type="card" height="200px">
+  <el-carousel :interval="4000" type="card" height="400px">
     <el-carousel-item v-for="item in 6" :key="item">
       <h3>{{ item }}</h3>
     </el-carousel-item>
