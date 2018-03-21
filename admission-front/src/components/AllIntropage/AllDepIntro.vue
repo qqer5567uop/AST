7bd386b3f9aee313b0527b2d31c60263abe811ec
weak chicken <852833062@qq.com>
2018-03-21 18:50:53 +0800

    3.20

--- admission-front/src/components/AllIntropage/AllDepIntro.vue
+++ admission-front/src/components/AllIntropage/AllDepIntro.vue
@@ -2,34 +2,133 @@
   <div>
     <el-row class="row" id="card">
 
-      <el-card :body-style="{ padding: '50px' }"  class="card" >
-        <img src="" class="image">
-        <div style="padding: 14px;">
-          <button v-on:click="handleCard('KeXie')">部门1</button>
+      <el-card :body-style="{ padding: '10px' }"  class="card" >
+        <div style="padding: 5px;">
+          <img src="" class="image">
+          <button v-on:click="handleCard('KeXie')">科协技术部</button>
         </div>
       </el-card>
 
-      <el-card :body-style="{ padding: '50px' }" class="card">
+      <el-card :body-style="{ padding: '10px' }" class="card">
         <img src="" class="image">
-        <div style="padding: 14px;">
-          <span>部门2</span>
+        <div style="padding: 5px;">
+          <span>科协赛事部</span>
         </div>
       </el-card>
 
-      <el-card :body-style="{ padding: '50px' }" class="card">
+      <el-card :body-style="{ padding: '10px' }" class="card">
         <img src="" class="image">
-        <div style="padding: 14px;">
-          <span>部门3</span>
+        <div style="padding: 5px;">
+          <span>小红帽</span>
         </div>
       </el-card>
 
-      <el-card :body-style="{ padding: '50px' }" class="card">
+      <el-card :body-style="{ padding: '10px' }" class="card">
         <img src="" class="image">
-        <div style="padding: 14px;">
-          <span>部门4</span>
+        <div style="padding: 5px;">
+          <span>新闻中心</span>
+        </div>
+      </el-card>   
 
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>办公室</span>
+        </div>
+      </el-card>  
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>礼仪队</span>
         </div>
       </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>辩论队</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>主持人队</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>文艺部</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>学术部</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>外联部</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>女生部</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>权益部</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>启明书屋</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>组织部</span>
+        </div>
+      </el-card>
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>心理部</span>
+        </div>
+      </el-card>                          
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>社会实践部</span>
+        </div>
+      </el-card> 
+
+
+      <el-card :body-style="{ padding: '10px' }" class="card">
+        <img src="" class="image">
+        <div style="padding: 5px;">
+          <span>宣传部</span>
+        </div>
+      </el-card>    
+
 
     </el-row>
   </div>
@@ -38,7 +137,6 @@
 
 <script>
   import ElRow from "element-ui/packages/row/src/row";
-
   export default {
     components: {ElRow},
     data() {
@@ -69,14 +167,14 @@
   .card{
     border-radius: 20px;
     margin: 60px;
-    width:300px;
-    height: 300px;
+    width:110px;
+    height: 100px;
     float: left;
   }
   .bottom {
     margin-bottom: 0;
     margin-right: 0;
-    line-height: 12px;
+    line-height: 10px;
   }
   .image {
     width: 100%;
