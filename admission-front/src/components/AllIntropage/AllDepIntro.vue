5ccc9903f346c4c2a2a25f226ce3e624bcd942a6
weak chicken <852833062@qq.com>
2018-03-21 21:24:23 +0800

    3.21

--- admission-front/src/components/AllIntropage/AllDepIntro.vue
+++ admission-front/src/components/AllIntropage/AllDepIntro.vue
@@ -18,105 +18,105 @@
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">小红帽</span>
+          <span v-on:click="handleCard('XiaoHongMao')"><img src="/static/AST.jpg" class="image">小红帽</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">新闻中心</span>
+          <span v-on:click="handleCard('XinWenZhongXin')"><img src="/static/AST.jpg" class="image">新闻中心</span>
         </div>
       </el-card>   
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">办公室</span>
+          <span v-on:click="handleCard('BanGongShi')"><img src="/static/AST.jpg" class="image">办公室</span>
         </div>
       </el-card>  
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">礼仪队</span>
+          <span v-on:click="handleCard('LiYiDui')"><img src="/static/AST.jpg" class="image">礼仪队</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">辩论队</span>
+          <span v-on:click="handleCard('BianLunDui')"><img src="/static/AST.jpg" class="image">辩论队</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">主持人队</span>
+          <span v-on:click="handleCard('ZhuChiRenDui')"><img src="/static/AST.jpg" class="image">主持人队</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">文艺部</span>
+          <span v-on:click="handleCard('WenYiBu')"><img src="/static/AST.jpg" class="image">文艺部</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">学术部</span>
+          <span v-on:click="handleCard('XueShuBu')"><img src="/static/AST.jpg" class="image">学术部</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">外联部</span>
+          <span v-on:click="handleCard('WaiLianBu')"><img src="/static/AST.jpg" class="image">外联部</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">女生部</span>
+          <span v-on:click="handleCard('NvShengBu')"><img src="/static/AST.jpg" class="image">女生部</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">权益部</span>
+          <span v-on:click="handleCard('QuanYiBu')"><img src="/static/AST.jpg" class="image">权益部</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">启明书屋</span>
+          <span v-on:click="handleCard('QiMingShuWu')"><img src="/static/AST.jpg" class="image">启明书屋</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">组织部</span>
+          <span v-on:click="handleCard('ZuZhiBu')"><img src="/static/AST.jpg" class="image">组织部</span>
         </div>
       </el-card>
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">心理部</span>
+          <span v-on:click="handleCard('XinLiBu')"><img src="/static/AST.jpg" class="image">心理部</span>
         </div>
       </el-card>                          
 
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">社会实践部</span>
+          <span v-on:click="handleCard('SheHuiShiJianBu')"><img src="/static/AST.jpg" class="image">社会实践部</span>
         </div>
       </el-card> 
 
@@ -124,7 +124,7 @@
       <el-card :body-style="{ padding: '0px' }" class="card">
         <img src="" class="image">
         <div style="padding: 6px;text-align:center;">
-          <span v-on:click="handleCard('KeXie')"><img src="/static/AST.jpg" class="image">宣传部</span>
+          <span v-on:click="handleCard('XuanChuanBu')"><img src="/static/AST.jpg" class="image">宣传部</span>
         </div>
       </el-card>    
 
@@ -154,6 +154,54 @@
             break;
           case 'KeXieSaiShiBu':
             this.$router.push('/KeXieSaiShiBu');
+            break;
+          case 'XiaoHongMao':
+            this.$router.push('/XiaoHongMao');
+            break;
+          case 'XinWenZhongXin':
+            this.$router.push('/XinWenZhongXin');
+            break;
+          case 'BanGongShi':
+            this.$router.push('/BanGongShi');
+            break;
+          case 'LiYiDui':
+            this.$router.push('/LiYiDui');
+            break;
+          case 'BianLunDui':
+            this.$router.push('/BianLunDui');
+            break;
+          case 'ZhuChiRenDui':
+            this.$router.push('/ZhuChiRenDui');
+            break;
+          case 'WenYiBu':
+            this.$router.push('/WenYiBu');
+            break;
+          case 'XueShuBu':
+            this.$router.push('/XueShuBu');
+            break;
+          case 'WaiLianBu':
+            this.$router.push('/WaiLianBu');
+            break;
+          case 'NvShengBu':
+            this.$router.push('/NvShengBu');
+            break;
+          case 'QuanYiBu':
+            this.$router.push('/QuanYiBu');
+            break;
+          case 'QiMingShuWu':
+            this.$router.push('/QiMingShuWu');
+            break;
+          case 'ZuZhiBu':
+            this.$router.push('/ZuZhiBu');
+            break;
+          case 'XinLiBu':
+            this.$router.push('/XinLiBu');
+            break;
+          case 'SheHuiShiJianBu':
+            this.$router.push('/SheHuiShiJianBu');
+            break;
+          case 'XuanChuanBu':
+            this.$router.push('/XuanChuanBu');
             break;
           default:
             this.$router.push('/Home');
--- admission-front/src/router/index.js
+++ admission-front/src/router/index.js
@@ -9,6 +9,22 @@ import Display from '../components/Display'
 
 import KeXieJiShuBu from '../components/AllIntropage/KeXieJiShuBu'
 import KeXieSaiShiBu from '../components/AllIntropage/KeXieSaiShiBu'
+import XiaoHongMao from '../components/AllIntropage/XiaoHongMao'
+import XinWenZhongXin from '../components/AllIntropage/XinWenZhongXin'
+import BanGongShi from '../components/AllIntropage/BanGongShi'
+import LiYiDui from '../components/AllIntropage/LiYiDui'
+import BianLunDui from '../components/AllIntropage/BianLunDui'
+import ZhuChiRenDui from '../components/AllIntropage/ZhuChiRenDui'
+import WenYiBu from '../components/AllIntropage/WenYiBu'
+import XueShuBu from '../components/AllIntropage/XueShuBu'
+import WaiLianBu from '../components/AllIntropage/WaiLianBu'
+import NvShengBu from '../components/AllIntropage/NvShengBu'
+import QuanYiBu from '../components/AllIntropage/QuanYiBu'
+import QiMingShuWu from '../components/AllIntropage/QiMingShuWu'
+import ZuZhiBu from '../components/AllIntropage/ZuZhiBu'
+import XinLiBu from '../components/AllIntropage/XinLiBu'
+import SheHuiShiJianBu from '../components/AllIntropage/SheHuiShiJianBu'
+import XuanChuanBu from '../components/AllIntropage/XuanChuanBu'
 
 
 export default [
@@ -30,4 +46,52 @@ export default [
     {
       path: '/KeXieSaiShiBu',component:KeXieSaiShiBu
     },
+    {
+      path: '/XiaoHongMao',component:XiaoHongMao
+    },
+    {
+      path: '/XinWenZhongXin',component:XinWenZhongXin
+    },
+    {
+      path: '/BanGongShi',component:BanGongShi
+    },
+    {
+      path: '/LiYiDui',component:LiYiDui
+    },
+    {
+      path: '/BianLunDui',component:BianLunDui
+    },
+    {
+      path: '/ZhuChiRenDui',component:ZhuChiRenDui
+    },
+    {
+      path: '/WenYiBu',component:WenYiBu
+    },
+    {
+      path: '/XueShuBu',component:XueShuBu
+    },
+    {
+      path: '/WaiLianBu',component:WaiLianBu
+    },
+    {
+      path: '/NvShengBu',component:NvShengBu
+    },
+    {
+      path: '/QuanYiBu',component:QuanYiBu
+    },
+    {
+      path: '/QiMingShuWu',component:QiMingShuWu
+    },
+    {
+      path: '/ZuZhiBu',component:ZuZhiBu
+    },
+    {
+      path: '/XinLiBu',component:XinLiBu
+    },
+    {
+      path: '/SheHuiShiJianBu',component:SheHuiShiJianBu
+    },
+    {
+      path: '/XuanChuanBu',component:XuanChuanBu
+    }
     ]
