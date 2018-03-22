994a499bf9c74461a2f6663361dea0d418c84037
TimDeng <milkybird98@outlook.com>
2018-03-22 23:25:25 +0800

    add some descriptions

--- admission-back/routes/information.js
+++ admission-back/routes/information.js
@@ -6,85 +6,86 @@ let urlencodedParser = bodyParser.urlencoded({ extended: false });
 let fs = require('fs');
 
 router.post('/', urlencodedParser, async function (req, res, next) {
-    // 获取req.body传来的信息，暂存在StudentData中
-    let StudentData = {
-        name: req.body.name,
-        uid: req.body.uid,
-        sex: req.body.sex,
-        class: req.body.class,
-        phone: req.body.phone,
-        FirstExcept: req.body.FirstExcept,
-        SecondExcept: req.body.SecondExcept,
-        AdjustedOrNot: req.body.AdjustedOrNot,
-        SelfIntroduction: req.body.SelfIntroduction,
-    }
-    // 函数验证StudentData是否符合规范，不符合则返回400（请求错误） 
-    // 同是验证是否存在重复录入的情况
-    if (!_validateStudentData(StudentData)) {
-        res.status(400).json({"msg":"Empty"})
-    } else {
-        let collection = await informationDB.getCollection();
-        collection.findOne({ name: StudentData.name }, function (err, data) {
-            if (!data) {
-                collection.insertOne({
-                    name: StudentData.name,
-                    uid: StudentData.uid,
-                    sex: StudentData.sex,
-                    class: StudentData.class,
-                    phone: StudentData.phone,
-                    FirstExcept: StudentData.FirstExcept,
-                    SecondExcept: StudentData.SecondExcept,
-                    AdjustedOrNot: StudentData.AdjustedOrNot,
-                    SelfIntroduction: StudentData.SelfIntroduction,
-                }, function () {
-                    res.status(200).json({"msg":"Create Successfully"})
-                })
-            }
-            else {
-                //console.log(data);
-                if (req.body.uid === data.uid ) {
-                    collection.save({
-                        "_id": data._id,
-                        "name": StudentData.name,
-                        "uid": StudentData.uid,
-                        "sex": StudentData.sex,
-                        "class": StudentData.class,
-                        "phone": StudentData.phone,
-                        "FirstExcept": StudentData.FirstExcept,
-                        "SecondExcept": StudentData.SecondExcept,
-                        "AdjustedOrNot": StudentData.AdjustedOrNot,
-                        "SelfIntroduction": StudentData.SelfIntroduction,
-                    }, function () {
-                        res.status(200).json({"msg":"Change Successfully"});
-                    })
-                } else {
-                    res.status(400).json({"msg":"Dump"})
-                }
-            }
-        })
-    }
+	// 获取req.body传来的信息，暂存在StudentData中
+	let StudentData = {
+		name: req.body.name,
+		uid: req.body.uid,
+		sex: req.body.sex,
+		class: req.body.class,
+		phone: req.body.phone,
+		FirstExcept: req.body.FirstExcept,
+		SecondExcept: req.body.SecondExcept,
+		AdjustedOrNot: req.body.AdjustedOrNot,
+		SelfIntroduction: req.body.SelfIntroduction,
+	}
+	// 函数验证StudentData是否符合规范，不符合则返回400（请求错误） 
+	if (!_validateStudentData(StudentData)) {
+		res.status(400).json({ "msg": "Empty" })
+	} else {
+		let collection = await informationDB.getCollection();
+		// 验证数据库中是否存在同姓名的数据，如果不存在这新建，否则检测学号是否相同
+		collection.findOne({ name: StudentData.name }, function (err, data) {
+			if (!data) {
+				collection.insertOne({
+					name: StudentData.name,
+					uid: StudentData.uid,
+					sex: StudentData.sex,
+					class: StudentData.class,
+					phone: StudentData.phone,
+					FirstExcept: StudentData.FirstExcept,
+					SecondExcept: StudentData.SecondExcept,
+					AdjustedOrNot: StudentData.AdjustedOrNot,
+					SelfIntroduction: StudentData.SelfIntroduction,
+				}, function () {
+					res.status(200).json({ "msg": "Create Successfully" })
+				})
+			}
+			else {
+				//console.log(data);
+				// 如果学号相同，则修改数据，否则认为是姓名或学号输入错误，出现重复
+				if (req.body.uid === data.uid) {
+					collection.save({
+						"_id": data._id,
+						"name": StudentData.name,
+						"uid": StudentData.uid,
+						"sex": StudentData.sex,
+						"class": StudentData.class,
+						"phone": StudentData.phone,
+						"FirstExcept": StudentData.FirstExcept,
+						"SecondExcept": StudentData.SecondExcept,
+						"AdjustedOrNot": StudentData.AdjustedOrNot,
+						"SelfIntroduction": StudentData.SelfIntroduction,
+					}, function () {
+						res.status(200).json({ "msg": "Change Successfully" });
+					})
+				} else {
+					res.status(400).json({ "msg": "Dump" })
+				}
+			}
+		})
+	}
 });
 // 验证StudentData是否格式合法，具有必须字段
 function _validateStudentData(StudentData) {
-    if (StudentData.name === undefined || StudentData.name == "")
-        return false;
-    if (StudentData.uid === undefined || StudentData.uid == "")
-        return false;
-    if (StudentData.sex === undefined || StudentData.sex == "")
-        return false;
-    if (StudentData.class === undefined || StudentData.class == "")
-        return false;
-    if (StudentData.phone === undefined || StudentData.phone == "")
-        return false;
-    if (StudentData.FirstExcept === undefined || StudentData.FirstExcept == "")
-        return false;
-    if (StudentData.SecondExcept === undefined || StudentData.SecondExcept == "")
-        return false;
-    if (StudentData.AdjustedOrNot === undefined || StudentData.AdjustedOrNot == "")
-        return false;
-    if ((StudentData.SelfIntroduction === undefined || StudentData.SelfIntroduction == "") || ((StudentData.SelfIntroduction).length > 300))
-        return false;
-    return true;
+	if (StudentData.name === undefined || StudentData.name == "")
+		return false;
+	if (StudentData.uid === undefined || StudentData.uid == "")
+		return false;
+	if (StudentData.sex === undefined || StudentData.sex == "")
+		return false;
+	if (StudentData.class === undefined || StudentData.class == "")
+		return false;
+	if (StudentData.phone === undefined || StudentData.phone == "")
+		return false;
+	if (StudentData.FirstExcept === undefined || StudentData.FirstExcept == "")
+		return false;
+	if (StudentData.SecondExcept === undefined || StudentData.SecondExcept == "")
+		return false;
+	if (StudentData.AdjustedOrNot === undefined || StudentData.AdjustedOrNot == "")
+		return false;
+	if ((StudentData.SelfIntroduction === undefined || StudentData.SelfIntroduction == "") || ((StudentData.SelfIntroduction).length > 300))
+		return false;
+	return true;
 }
 
 module.exports = router;
--- admission-back/routes/lookfor.js
+++ admission-back/routes/lookfor.js
@@ -4,15 +4,20 @@ let router = express.Router();
 let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+/*  查询学生报名数据  */
 router.post('/', urlencodedParser, async function (req, res, next) {
 	let collection = await informationDB.getCollection();
+
+	// 检测数据是否符合
 	if (!_validateStudentData(req.body)) {
-		res.status(400).json({"msg":"Empty"})
+		res.status(400).json({ "msg": "Empty" })
 	} else {
-		collection.findOne({ uid: req.body.uidnph.substr(0,10) }, function (err, data) {
+		// 依据学号查询是否有数据
+		collection.findOne({ uid: req.body.uidnph.substr(0, 10) }, function (err, data) {
 			if (!data) {
-				res.status(400).json({"msg":"Not found"})
+				res.status(400).json({ "msg": "Not found" })
 			} else {
+				// 验证身份信息是否匹配
 				if (data.phone.substr(-4) === req.body.uidnph.substr(-4)) {
 					res.status(200).json({
 						name: data.name,
@@ -26,7 +31,7 @@ router.post('/', urlencodedParser, async function (req, res, next) {
 						SelfIntroduction: data.SelfIntroduction,
 					});
 				} else {
-					res.status(400).json({"msg":"Not matched"})
+					res.status(400).json({ "msg": "Not matched" })
 				}
 			}
 		})
