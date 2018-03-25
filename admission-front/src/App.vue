a274a9e4632cdfaafcb4bf4c11349e9ac71ea100
weak chicken <852833062@qq.com>
2018-03-25 22:03:47 +0800

    3.25

---
+++ admission-front/src/App.vue
@@ -0,0 +1,100 @@
+<template>
+
+  <div id="app">
+    <!-- 头部导航 -->
+    <header class="header">
+
+      <el-row>
+        <el-col :span="30">
+          <el-menu default-active="4" class="el-menu-demo" style="width: 400px" mode="horizontal" @select="handleMenu">
+            <el-menu-item index="home">了解我们</el-menu-item>
+            <el-menu-item index="depintro">部门介绍</el-menu-item>
+            <el-menu-item index="display">风向展示</el-menu-item>
+            <el-menu-item index="form">加入我们</el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </header>
+    <div style="position: relative;height: 60px;width: 100%;">
+
+    </div>
+
+    <main>
+      <!-- 右侧主内容区 -->
+      <div>
+        <transition name="fade">
+          <router-view class="view"></router-view>
+        </transition>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import Element from 'element-ui'
+  import 'element-ui/lib/theme-chalk/index.css'
+
+  Vue.use(Element)
+
+  export default {
+    name: 'app',
+    data: function (){
+      return {
+        active:true
+      }
+    },
+    created: function(){
+      this.$router.push('');
+    },
+    methods: {
+      handleMenu:function (key) {
+        switch (key) {
+          case 'home':
+            this.$router.push('/Home');
+            break;
+          case 'depintro':
+            this.$router.push('/AllDepintro');
+            break;
+          case 'display':
+            this.$router.push('/Display');
+            break;
+          case 'form':
+            this.$router.push('/Form');
+            break;
+          default:
+            this.$router.push('/Home');
+        }
+        console.log(key)
+
+      }
+    }
+  }
+</script>
+
+
+<style>
+  body{margin: 0;}
+  #app {
+    min-width: 100%;
+    margin: 0 auto;
+    font-family: "Helvetica Neue","PingFang SC",Arial,sans-serif;
+  }
+  /* 头部导航 */
+  header{
+
+    min-width: 100%;
+    transition: all 0.5s ease;
+    border-top: solid 4px #3091F2;
+    background-color: #fff;
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,.12),0 0 6px 0 rgba(0,0,0,.04);
+  }
+
+
+  /* 主内容区 */
+  main{
+    width: 100%;
+    background-color: #FCFCFC;
+  }
+
+</style>
---
+++ admission-front/src/components/Readme.md
@@ -0,0 +1,10 @@
+### 了解我们-Home
+### 部门介绍-AllDepIntro
+#### 各个部门的单独介绍AllIntroPage目录
+#### 
+
+
+
+### 风采展示-Display
+### 加入我们-Form
+
